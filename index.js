0ccd9bf64d5ccad7d148622834cd6ac382ef25fa
James <jamestur@msn.com>
2026-02-06 16:26:21 -0700

    exclusive

---
+++ index.js
@@ -0,0 +1,33 @@
+// template_z94r6am
+// service_f5mumen
+// 7XGCy_rpvXcyWBWD0
+
+function contact(event) {
+    event.preventDefault();
+     const loading = document.querySelector(".modal__overlay--loading");
+    const success = document.querySelector(".modal__overlay--success");
+    loading.classList += " modal__overlay--visible";
+    
+    emailjs
+        .sendForm(
+            "service_f5mumen",
+            "template_z94r6am",
+            event.target,
+            "7XGCy_rpvXcyWBWD0"
+        ) .then(() => {
+            loading.classList.remove("modal__overlay--visible");
+        success.classList += " modal__overlay--visible";
+        }).catch(() => {
+            loading.classList.remove("modal__overlay--visible");
+            alert("Something went wrong, please try again.");
+        })
+  }
+
+  let ismodalOpen = false;
+  function toggleModal() {
+    if (ismodalOpen) {
+        return document.body.classList.remove("modal--open");
+    }
+    ismodalOpen = !ismodalOpen;
+    document.body.classList += " modal--open";}
+
