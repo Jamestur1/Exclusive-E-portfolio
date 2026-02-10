b801a47d426fb6a3209c57d11c92e8dd63d434ea
James <jamestur@msn.com>
2026-02-10 09:34:25 -0700

    exclusive

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 
 let ismodalOpen = false;
 let contrastToggle = false;
+
 const scaleFactor = 1 / 20;
 
 function moveBackground(event) {
@@ -11,10 +12,10 @@ function moveBackground(event) {
     const x = event.clientX * scaleFactor;
     const y = event.clientY * scaleFactor;
 
-    for (let i = 0; i < shapes.length ++i) {
+    for (let i = 0; i < shapes.length; ++i) {
         const isOdd = i % 2 !== 0;
         const boolInt= isOdd ? -1 : 1;
-       shapes[i].styles.transform = `translate(${x * boolInt }px, ${y * boolInt}px)`
+       shapes[i].style.transform = `translate(${x * boolInt }px, ${y * boolInt}px)`
     }
 }
 
